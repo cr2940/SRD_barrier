fa2480d93fc83c0a00ddac7bf8771c17df05de78
cr2940 <36740525+cr2940@users.noreply.github.com>
2021-09-02 15:37:19 -0400

    fix function arg syntax
    riemanntype

--- PyClaw_SRD_Codes/classic/V/step2ds.f90
+++ PyClaw_SRD_Codes/classic/V/step2ds.f90
@@ -848,7 +848,7 @@
        wall(2) = 1.d0
        wall(3) = 1.d0
        if (hR.le.drytol) then
-          call riemanntype(hL,hL,uL,-uL,hstar,s1m,&
+          call riemanntype(hL,hL,uL,-uL,hstar,uL,s1m,&
           s2m,rare1,rare2,1,drytol,g)
           hstartest=max(hL,hstar)
           if (hstartest+bL.lt.bR) then !right state should become ghost values that mirror left for wall problem
@@ -865,7 +865,7 @@
              bR=hL+bL
           endif
        elseif (hL.le.drytol) then ! right surface is lower than left topo
-          call riemanntype(hR,hR,-uR,uR,hstar,s1m,s2m,&
+          call riemanntype(hR,hR,-uR,uR,hstar,uL,s1m,s2m,&
              rare1,rare2,1,drytol,g)
           hstartest=max(hR,hstar)
           if (hstartest+bR.lt.bL) then  !left state should become ghost values that mirror right
